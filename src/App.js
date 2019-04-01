b4e9bf01e675e4b99eece0e9bea978b920a21aa7
francistse23 <wtse.ucsd@gmail.com>
2019-04-01 14:29:58 -0700

    Set up constants for app width and main color. Implemented method 'initialize' for setting up initial page result.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import axios from 'axios';
+import styled from 'styled-components';
+import { maxAppWidth } from './lib/constants';
+
+const AppPageContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  min-height: 100vh;
+  max-width: ${maxAppWidth}px;
+`
+
+const key = process.env.REACT_APP_API_KEY || 'dc6zaTOxFJmzC';
+
+
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      offset: 0,
+      results: [],
+      total: 0,
+      type: 'gifs',
+    }
+  }
+  // retrieves all trending GIFs/Stickers from GIPHY
+  initialize = () => {
+    // GIPHY's public beta key, didn't use env since it's public. could set up as env var if needed
+    // the public beta key does not return more than 25 result despite setting limit in query string
+    // tested with my own API key
+    axios.get(`http://api.giphy.com/v1/${this.state.type}/trending?api_key=${key}&offset=${this.state.offset}`)
+        .then( res => {
+            this.setState({ 
+                results: res.data.data,
+                total: res.data.pagination.total_count
+            })
+        });
+  }
+  componentDidMount(){
+    // by default, the page will load trending content on start
+    this.initialize();
+
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <AppPageContainer>
+        <code>Hello</code>
+      </AppPageContainer>
     );
   }
 }
---
+++ src/lib/constants.js
@@ -0,0 +1,6 @@
+// max width in px
+export const maxAppWidth = 1600;
+
+
+// colors
+export const eazeBlue = '#00a6e0';
